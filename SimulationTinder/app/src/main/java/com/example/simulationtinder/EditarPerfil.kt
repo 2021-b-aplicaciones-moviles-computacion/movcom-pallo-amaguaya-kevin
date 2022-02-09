c7787d9cdaa8fc2cbe7943d1b98ded08e957f40c
Kevin Pallo <kevin.pallo@epn.edu.ec>
2022-02-08 23:38:22 -0500

    Diseño de interface 2
    Editar perfil falta poco

---
+++ SimulationTinder/app/src/main/java/com/example/simulationtinder/EditarPerfil.kt
@@ -0,0 +1,24 @@
+package com.example.simulationtinder
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+
+class EditarPerfil : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_editar_perfil)
+
+        //DA1E1E Color de la flecha
+
+        //BOTON REGRESAR BIOGRAFIA
+        val btn_backBiografia = findViewById<ImageButton>(R.id.imageButton_BackBibliografia)
+        btn_backBiografia.setOnClickListener {
+            startActivity(Intent(this, Biografia::class.java))
+        }
+
+    }
+
+
+}
--- SimulationTinder/app/src/main/java/com/example/simulationtinder/PerfilUsuario.kt
+++ SimulationTinder/app/src/main/java/com/example/simulationtinder/PerfilUsuario.kt
@@ -15,6 +15,15 @@ class PerfilUsuario : AppCompatActivity() {
         btn_backInicio.setOnClickListener {
             startActivity(Intent(this, Inicio::class.java))
         }
-        
+
+        var btn_perfilUsuario = findViewById<ImageButton>(R.id.imageButton_perfil)
+        btn_perfilUsuario.setOnClickListener {
+            startActivity(Intent(this, Biografia::class.java))
+        }
+
+        var btn_editarPerfil = findViewById<ImageButton>(R.id.imageButton_editarPerfil2)
+        btn_editarPerfil.setOnClickListener {
+            startActivity(Intent(this, EditarPerfil::class.java))
+        }
     }
 }
