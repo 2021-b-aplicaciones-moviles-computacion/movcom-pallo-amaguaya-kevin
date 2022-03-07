abad0dccae410117f16a15e956038dabfbd90ca1
Adrian Laje <adrian.laje@epn.edu.ec>
2022-03-06 22:25:26 -0500

    correcciones y explorar

--- Proyecto/app/src/main/java/com/example/istock/HomeActivity.kt
+++ Proyecto/app/src/main/java/com/example/istock/HomeActivity.kt
@@ -3,6 +3,7 @@ package com.example.istock
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.istock.extensions.Extensions.toast
 import com.example.istock.utils.FirebaseUtils.firebaseAuth
 import com.example.istock.views.CreateAccountActivityy
@@ -13,32 +14,23 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        BottomNavigationView.OnNavigationItemSelectedListener { item ->
-            when(item.itemId) {
-                R.id.item1 -> {
-                    // Respond to navigation item 1 click
-                    true
+        bottom_navigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.page_1 -> {
+
                 }
-                R.id.item2 -> {
-                    // Respond to navigation item 2 click
-                    true
+                R.id.page_2 -> {
                 }
-                R.id.item3 -> {
-                    // Respond to navigation item 2 click
-                    true
+                R.id.page_3 -> {
+                    val i = Intent(this, ExplorarActivity::class.java)
+                    startActivity(i);
                 }
-                R.id.item4 -> {
-                    // Respond to navigation item 2 click
-                    true
+                R.id.page_4 -> {
+                    val i = Intent(this, ProfileActivity::class.java)
+                    startActivity(i);
                 }
-                else -> false
             }
-        }
-        btnSignOut.setOnClickListener {
-            firebaseAuth.signOut()
-            startActivity(Intent(this, CreateAccountActivityy::class.java))
-            toast("signed out")
-            finish()
+            true
         }
     }
 }
